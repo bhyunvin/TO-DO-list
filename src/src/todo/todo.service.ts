3bf8294a061f5c3ae36f41fbc0e9196d3a4ab53c
baekjonghyun <bhyunvin@gaeasoft.co.kr>
2025-10-10 17:25:01 +0900

    todo 관련 작업 중

--- src/src/todo/todo.service.ts
+++ src/src/todo/todo.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Repository, In } from 'typeorm';
+import { Repository, In, LessThanOrEqual, IsNull, Not } from 'typeorm';
 import { TodoEntity } from './todo.entity';
 import { CreateTodoDto, UpdateTodoDto } from './todo.dto';
 import { setAuditColumn, AuditSettings } from '../utils/auditColumns';
@@ -17,10 +17,30 @@ export class TodoService {
   // 특정 사용자의 특정 날짜의 모든 ToDo 항목을 조회합니다.
   async findAll(userSeq: number, todoDate: string): Promise<TodoEntity[]> {
     return this.todoRepository.find({
-      where: {
-        userSeq,
-        todoDate,
-        delYn: 'N', // 삭제되지 않은 항목만 조회합니다.
+      where: [
+        // 1. 완료되지 않은 항목: 조회일(todoDate) 이전에 생성된 모든 미완료 항목을 포함합니다.
+        {
+          userSeq,
+          todoDate: LessThanOrEqual(todoDate),
+          completeDtm: IsNull(),
+          delYn: 'N',
+        },
+        // 2. 완료된 항목: 정확히 조회일(todoDate)에 해당하는 완료된 항목만 포함합니다.
+        {
+          userSeq,
+          todoDate: todoDate,
+          completeDtm: Not(IsNull()),
+          delYn: 'N',
+        },
+      ],
+      order: {
+        // 완료되지 않은 항목(completeDtm이 null)을 먼저, 그 다음 완료된 항목을 오름차순으로 정렬합니다.
+        completeDtm: {
+          direction: 'DESC',
+          nulls: 'FIRST',
+        },
+        // 같은 조건 내에서는 최신 항목(todoSeq가 큰 값)이 위로 오도록 내림차순 정렬합니다.
+        todoSeq: 'DESC',
       },
     });
   }
