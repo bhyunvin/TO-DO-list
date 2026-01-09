3050f3d3c5361dbd3c7e2a9c845f5a1b857e3b80
baekjonghyun <bhyunvin@gaeasoft.co.kr>
2026-01-09 10:15:28 +0900

    welcome 라우터 반환 형식 변경

--- src/src/welcome.controller.ts
+++ src/src/welcome.controller.ts
@@ -3,7 +3,7 @@ import { Controller, Get } from '@nestjs/common';
 @Controller()
 export class WelcomeController {
   @Get('/')
-  welcome(): string {
-    return 'Welcome to the Todo App API!';
+  welcome(): object {
+    return { message: 'Welcome to the Todo App API!', status: 'live' };
   }
 }
